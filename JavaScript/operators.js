a63e33a6cc7e94902c1047ef7e989dc358600d76
Mih1rGupt4 <mihirgupta422@gmail.com>
2023-01-06 10:56:50 +0530

    js

---
+++ JavaScript/operators.js
@@ -0,0 +1,15 @@
+console.log('operators')
+n1=100
+n2=200
+res = n1+n2
+console.log('result of addition is ', res)
+
+res = n1>n2
+console.log(n1,' > ',n2, ' : ', res)
+
+n3=2
+n4="2"
+console.log('n3==n4 ', (n3==n4))
+console.log('n3===n4 ', (n3===n4))
+console.log('n3>=n4', (n3>=n4))
+console.log('n1 <= n2', (n1<=n2))
